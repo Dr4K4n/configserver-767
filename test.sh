aa3d1a718a7d12ea2ecff5bdaa5be01f70bcbad6
Stefan Erichsen <stefan@balticfinance.com>
2017-11-02 08:56:57 +0100

    extented test script to start app and include result of encryption in debug output

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mvn clean package
+java -jar target/configserver-0.0.1-SNAPSHOT.jar &
+sleep 8
 input=encrypt-me
 hash=$(curl -s -X POST --data-urlencode $input http://localhost:8888/encrypt)
 output=$(curl -s -X POST --data-urlencode $hash http://localhost:8888/decrypt)
@@ -6,5 +9,5 @@ if [ "$input" = "$output" ]
 then
     echo "Test OK"
 else
-    echo "Test FAILED: $input -> $output"
+    echo "Test FAILED: $input -> $hash -> $output"
 fi
